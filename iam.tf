acdc4f30aca5e19aef3f2bb0d80406aca4af4db0
Lizz1102 <lizz05110@gmail.com>
2023-08-16 16:39:03 -0400

    Add codepipeline role

---
+++ iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "tf-codepipeline-role" {
+  name = "tf-codepipeline-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
